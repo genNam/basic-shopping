0b697e79edf4389debaa1434d001dce65a7ff8e2
genNam <wkwkuu8@gmail.com>
2025-12-11 04:04:25 +0900

    feat: 엔티티들이 공통으로 가져야할 필드를 정의한 클래스 구현

---
+++ src/main/java/com/kt/common/support/BaseAuditEntity.java
@@ -0,0 +1,22 @@
+package com.kt.common.support;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditEntity {
+
+	@CreatedBy
+	protected Long createdBy;
+
+	@LastModifiedBy
+	protected Long updatedBy;
+
+}

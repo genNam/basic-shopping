aa3bd4aca211ac5b04db9b013d872cc490cb8dcc
genNam <wkwku8@gmail.com>
2025-12-11 03:52:35 +0900

    initial commit

---
+++ src/main/java/com/kt/basic_shopping/BasicShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.kt.basic_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicShoppingApplication.class, args);
+	}
+
+}

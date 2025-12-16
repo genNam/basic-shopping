886142dcad94d24ca2bcae20f1a68f11f1ed83c2
genNam <wkwkuu8@gmail.com>
2025-12-17 00:10:33 +0900

    feat: user 내 정보 조회 기능 구현

--- src/main/java/com/kt/dto/user/UserResponse.java
+++ src/main/java/com/kt/dto/user/UserResponse.java
@@ -1,6 +1,29 @@
 package com.kt.dto.user;
 
+import java.time.LocalDateTime;
+
+import com.kt.domain.user.User;
+
 public class UserResponse {
+
+	public record Detail(
+
+		String id,
+		String name,
+		String email,
+		LocalDateTime createdAt
+
+	){
+		public static Detail from(User user){
+
+			return new Detail(
+				user.getLoginId(),
+				user.getName(),
+				user.getEmail(),
+				user.getCreatedAt()
+			);
+		}
+	}
 
 
 }

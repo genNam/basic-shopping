68990e531683a5f7a1a90ac82401029a9a76527b
genNam <wkwku8@gmail.com>
2025-12-27 15:01:57 +0900

    feat : 히스토리 기능 구현

---
+++ src/main/java/com/kt/domain/history/History.java
@@ -0,0 +1,29 @@
+package com.kt.domain.history;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.kt.common.support.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class History extends BaseEntity {
+
+	@Enumerated(value = EnumType.STRING)
+	private HistoryType historyType;
+
+	private String content;
+
+	private Long userId;
+
+	public History(HistoryType type, String content, Long userId){
+		this.type = type;
+		this.content = content;
+		this.userId = userId;
+	}
+}

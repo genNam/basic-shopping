f4f5d21337d8d42f9ce13b665d36ef1f4f2406b4
genNam <wkwku8@gmail.com>
2026-01-15 02:07:40 +0900

    refactor : 리뷰 API 경로 어노테이션 수정

--- src/main/java/com/kt/controller/order/AdminOrderController.java
+++ src/main/java/com/kt/controller/order/AdminOrderController.java
@@ -21,7 +21,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("admin/orders")
+@RequestMapping("/admin/orders")
 @RequiredArgsConstructor
 public class AdminOrderController {
 
--- src/main/java/com/kt/controller/review/ReviewController.java
+++ src/main/java/com/kt/controller/review/ReviewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,14 +23,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/review")
+@RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
 	private final ReviewService reviewService;
 
 	//상품 리뷰 작성(사용자)
-	@PostMapping("/reviews")
+	@PostMapping
 	public ApiResult<Void> create(
 		@AuthenticationPrincipal CurrentUser currentUser,
 		@RequestBody @Valid ReviewRequest.Create request
@@ -39,20 +40,20 @@ public class ReviewController {
 	}
 
 	//상품 리뷰 조회(사용자)
-	@PostMapping("/reviews?productId")
+	@GetMapping
 	public ApiResult<List<ReviewResponse.UserSearch>> userSearch(
-		@PathVariable Long productId
+		@RequestParam Long productId
 	){
 		var result = reviewService.userSearch(productId);
 		return ApiResult.ok(result);
 	}
 
 	//상품 리뷰 수정(사용자)
-	@PutMapping("/reviews/{id}")
+	@PutMapping("/{id}")
 	public ApiResult<ReviewResponse.UserUpdate> userUpdate(
 		@AuthenticationPrincipal CurrentUser currentUser,
 		@RequestBody @Valid ReviewRequest.Update request,
-		@RequestParam Long id
+		@PathVariable Long id
 	){
 		var result = reviewService.userUpdate(id, currentUser.getId(), request);
 
@@ -60,19 +61,14 @@ public class ReviewController {
 	}
 
 	//상품 리뷰삭제(사용자)
-	@DeleteMapping("/reviews/{id}")
+	@DeleteMapping("/{id}")
 	public ApiResult<Void> delete(
 		@AuthenticationPrincipal CurrentUser currentUser,
-		@RequestParam Long id
+		@PathVariable Long id
 	){
 		reviewService.delete(currentUser.getId(),id);
 
 		return ApiResult.ok();
 	}
-
-	//상품리뷰조회(관리자)
-	//admin/reviews
-	//상품리뷰삭제(관리자)
-	//admin/reviews/{id}
 
 }
